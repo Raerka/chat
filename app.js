886dbcd38860793e8023dac8ddbf96f64070bade
Andrei_Kasmykou <Andrei_Kasmykou@epam.com>
2017-12-22 14:36:00 +0300

    HTML Template was added

--- app.js
+++ app.js
@@ -8,41 +8,39 @@ const log = require('./libs/log')(module);
 //Создание приложения
 //Функция для обработки всех приходящих запросов!!
 const app = express();
-app.set('port', config.get('port'));
 
-http.createServer(app).listen(app.get('port'), () => {
-  log.info('Express server listening on port ' + config.get('port'));
+//Шаблоны
+app.set('views', __dirname + '/templates');
+
+//Движок для шаблонов, может быть и другой.
+app.set('view engine', 'ejs');
+
+app.use(express.favicon());
+
+// Стандартный логгер. dev - это формат логгирования, есть другие
+if (app.get('env') === 'development') {
+  app.use(express.logger('dev'));
+}else {
+  app.use(express.logger('default'));
+}
+
+//Читает тело запроса (form, json). Данные доступны в req.body....
+app.use(express.bodyParser());
+
+//Парсер для cookie
+app.use(express.cookieParser('your secret here'));
+
+//Говорит о том какие запросы как будут обработаны в зависимости от URL и типа метода
+app.use(app.router);
+//Например для метода get
+app.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'chat application',
+    body: '<b>Hello<b>'
+  });
 });
 
-//Обработчик - Middleware - Для реагирования на запросы
-app.use((req, res, next) => {
-  if(req.url === '/'){
-    res.end('Hello');
-  }else {
-    next();
-  }
-});
-
-app.use((req, res, next) => {
-  if(req.url === '/forbidden'){
-    next(new Error('Oops, access denied!'));
-  }else {
-    next();
-  }
-});
-
-app.use((req, res, next) => {
-  if(req.url === '/test'){
-    res.end('Test');
-  }else {
-    next();
-  }
-});
-
-//Метод send - это метод который добавляется фреймворком express
-app.use((req, res) => {
-  res.send(404, 'Page is not found!!!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Обработчик ошибок - имеет четыре аргумента!!!
 app.use((err, req, res, next) => {
@@ -58,23 +56,45 @@ app.use((err, req, res, next) => {
 });
 
 
+http.createServer(app).listen(config.get('port'), () => {
+  log.info('Express server listening on port ' + config.get('port'));
+});
+
+
 
 // var routes = require('./routes');
 // var user = require('./routes/user');
 
-// // all environments
-// app.set('port', process.env.PORT || 3000);
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-// app.use(express.favicon());
-// app.use(express.logger('dev'));
-// app.use(express.bodyParser());
-// app.use(express.methodOverride());
-// app.use(express.cookieParser('your secret here'));
-// app.use(express.session());
-// app.use(app.router);
-// app.use(express.static(path.join(__dirname, 'public')));
-
-
 // app.get('/', routes.index);
 // app.get('/users', user.list);
+
+
+// //Обработчик - Middleware - Для реагирования на запросы
+// app.use((req, res, next) => {
+//   if(req.url === '/'){
+//     res.end('Hello');
+//   }else {
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   if(req.url === '/forbidden'){
+//     next(new Error('Oops, access denied!'));
+//   }else {
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   if(req.url === '/test'){
+//     res.end('Test');
+//   }else {
+//     next();
+//   }
+// });
+
+// //Метод send - это метод который добавляется фреймворком express
+// app.use((req, res) => {
+//   res.send(404, 'Page is not found!!!');
+// });
